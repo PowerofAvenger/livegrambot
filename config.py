8d04fc529d584218c838009d7732e668c8c1ed35
PowerofAvenger <88158323+PowerofAvenger@users.noreply.github.com>
2021-12-29 07:41:27 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,5 +4,5 @@ ban = "you were banned by the admin!"#ban message
 unban = "you were unbanned by the admin."#unban message
 text_message = "Message forwarded to mods of LEGION ✅"#answering_text to user
 banned = "you are blocked"#if admin has blocked a user
-main_id = -647246334 #id without quotes #can be group id
+main_id = 1425031223 #id without quotes #can be group id
 TOKEN = "5028494199:AAG_rAqBw7eA9hdNSe84Pi7_fXNk7SC-Kyk"#bot token
